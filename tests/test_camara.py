b376f96f83b654c3cbed0b55b8f7725fbe7c3db8
Liz12tg <lizethtg12@gmail.com>
2026-06-19 23:30:34 -0600

    correccion detección de color

--- tests/test_camara.py
+++ tests/test_camara.py
@@ -2,6 +2,6 @@ import os
 import cv2
 os.system("rpicam-still -n -t 100 -o /tmp/foto.jpg")
 if cv2.imread('/tmp/foto.jpg') is not None:
-    print("Prueba de cámara: OK")
+    print("Prueba de camara: OK")
 else:
     print("Error")
