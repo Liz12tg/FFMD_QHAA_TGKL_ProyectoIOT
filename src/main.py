ff544d4336dcca56de66a51309332d136835944c
alequher09 <alequev09@gmail.com>
2026-06-20 19:52:49 -0600

    separamos los servos de la camara(1)

--- src/main.py
+++ src/main.py
@@ -6,15 +6,17 @@ import cv2, numpy as np, os
 client = mqtt.Client()
 client.connect("localhost", 1883, 60)
 
-# Procesamiento
+# Adquisición de datos
 os.system("rpicam-still -n -t 100 -o /tmp/foto.jpg")
 frame = cv2.imread('/tmp/foto.jpg')
 h = np.mean(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)[:, :, 0])
 
+# Clasificación
 if h < 25 or h > 160: color = "ROJO"
 elif 30 < h < 85: color = "VERDE"
 else: color = "AZUL"
 
+# Publicación
 client.publish("iot/sensor/color", color)
 print(f"Publicado: {color}")
 client.disconnect()
